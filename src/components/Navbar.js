80344bc7a7c2d0ef71bfc879f6932d9b0f0ea7df
Cansu Öztaş <fcansuoztas@gmail.com>
2022-12-26 15:42:47 +0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
+import Navigation from "./Navbar/Navigation"
+import Auth from "./Navbar/Auth"
 
 function Navbar() {
   return (
- <nav>
-    navbar
+ <nav className="h-[3.75rem] flex items-center justify-between px-8">
+   <Navigation/>
+   <Auth/>
  </nav>
   )
 }
